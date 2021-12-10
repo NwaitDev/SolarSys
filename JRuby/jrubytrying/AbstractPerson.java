6bbb26aeb784ade7c7ed430e40c91552535198cb
NwaitDev <nathangallone@hotmail.com>
2021-12-10 11:21:57 +0100

    Tests persos de jruby

--- JRuby/jrubytrying/AbstractPerson.java
+++ JRuby/jrubytrying/AbstractPerson.java
@@ -1,12 +1,9 @@
-package jrubytrying ;
+package jrubytrying;
 
 public abstract class AbstractPerson {
-    //
-    public abstract void hello() ;
-    //
-    public void repeat(int nbtimes) {
-	if (nbtimes <= 0) System.out.println("I shut up;") ;
-	else for (int i = 0 ; i < nbtimes ; i++) hello() ;
+    public abstract void hello();
+
+    public void repeat(int num){
+        System.out.print(num);
     }
-    //
 }
--- JRuby/jrubytrying/Animal.java
+++ JRuby/jrubytrying/Animal.java
@@ -1,13 +1,14 @@
-package jrubytrying ;
+package jrubytrying;
 
 public class Animal {
-    //
-    private String name ;
-    //
-    public Animal(String name_0) { name = name_0 ; }
-    //
-    public String get_name() { return name ; }
-    //
-    public void eat() { System.out.println("Miam-miam bouffe-bouffe !") ; }
-    //
+
+    private String name;
+
+    public Animal(String param){
+        this.name = param;
+    }
+
+    public String identity(){
+        return name;
+    }
 }
