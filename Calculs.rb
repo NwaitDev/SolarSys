572e655a4e2371ba4867762dfdeee41277dd537a
NwaitDev <nathangallone@hotmail.com>
2021-12-20 22:35:52 +0100

    Implem conv coordonnées kepler -> cartésiennes

--- Calculs.rb
+++ Calculs.rb
@@ -3,62 +3,136 @@ def absoluteDayFromEpoch(y,m,d, ut=0)
     367*y - 7 * ( y + (m+9)/12 ) / 4 - 3 * ( ( y + (m-9)/7 ) / 100 + 1 ) / 4 + 275*m/9 + d - 730515 + ut/24
 end
 
-class CartPosition
-    attr_reader :x, :y, :z
-    def initialize(x,y,z)
-        @x = x
+def mu(referentialBody)
+    case referentialBody
+    when "Sun"
+        1.32712440018 * (10**20)
+    when "Mercury"
+        2.2032 * (10**13)
+    when "Venus"
+        3.24859 * (10**14)
+    when "Earth"
+        3.986004418 * (10**14)
+    when "Mars"
+        4.282837 * (10**13)
+    when "Jupiter"
+        1.26686534 * (10**17)
+    when "Saturn"
+        3.7931187 * (10**16)
+    when "Uranus"
+        5.793939 * (10**15)
+    when "Neptune"
+        6.836529 * (10**15)
+    else
+        throw :NoCorrespondingCelestialBody
+    end
+end
+
+def newtonsMethod(guess, precision, function, derivativeOfFunction)
+    max_iteration = 30
+    current_iteration = 0
+    x = guess
+    f = function.call(x)
+    while(f.abs() > precision && current_iteration < max_iteration)
+        x = x - (f / derivativeOfFunction.call(x))
+        f = function.call(x)
+    end
+    x
+end
+
+def eccentricAnomaly(meanAnomaly,eccentricity)
+    meanAnomaly = meanAnomaly/360.0
+    meanAnomaly = 2*Math.pi*(meanAnomaly-Math.floor(meanAnomaly))
+
+    def F(estimatedExcentricAnomaly)
+        estimatedExcentricAnomaly-eccentricity*Math.sin(estimatedExcentricAnomaly)-meanAnomaly
+    end
+
+    def derOfF(estimatedExcentricAnomaly)
+        1-eccentricity*Math.cos(estimatedExcentricAnomaly)
+    end
+
+    if (eccentricity<0.8) 
+        guess=m 
+    else 
+        guess=Math.pi
+    end
+
+    newtonsMethod(guess, 0.0000001,:F,:derOfF)
+end
+
+def trueAnomaly(eccentricAnomaly,eccentricity)    
+    2*Math.atan2(Math.sqrt(1+eccentricity)*Math.sin(eccentricAnomaly/2),Math.sqrt(1-eccentricity)*Math.sin(eccentricAnomaly/2))
+end
+
+def distanceFromCentralBody(eccentricity,semiMajorAxis, eccentricAnomaly)
+    semiMajorAxis*(1-eccentricity*Math.cos(eccentricAnomaly))
+end
+
+def orbitalFrameCartesianCoord(semiMajorAxis, trueAnomaly, distanceFromCentralBody,mu,eccentricAnomaly)
+    factPos = distanceFromCentralBody
+    x = factPos * Math.cos(trueAnomaly)
+    y = factPos * Math.sin(trueAnomaly)
+    z = 0
+
+    factSpeed = Math.sqrt(mu*semiMajorAxis)/distanceFromCentralBody
+    xSpeed = factSpeed * (-Math.sin(eccentricAnomaly))
+    ySpeed = factSpeed * Math.sqrt(1-eccentricity**2)*Math.cos(eccentricAnomaly)
+    zSpeed = 0
+
+    CartesianCoord.new(x,y,z,xSpeed,ySpeed,zSpeed)
+end
+
+def bodyCentricFrameCartesianCoord(orbCoords,inclination,periapsisArg,ascendingNodeAngle)
+    
+    x = orbCoords.x*(Math.cos(periapsisArg)*Math.cos(ascendingNodeAngle) - Math.sin(periapsisArg)*Math.cos(inclination)*Math.sin(ascendingNodeAngle))
+        - orbCoords.y*(Math.sin(periapsisArg)*Math.cos(ascendingNodeAngle) + Math.cos(periapsisArg)*Math.cos(inclination)*Math.sin(ascendingNodeAngle))
+    y = orbCoords.x*(Math.cos(periapsisArg)*Math.sin(ascendingNodeAngle) + Math.sin(periapsisArg)*Math.cos(inclination)*Math.cos(ascendingNodeAngle))
+        + orbCoords.y*(Math.cos(periapsisArg)* Math.cos(inclination)* Math.cos(ascendingNodeAngle) - Math.sin(periapsisArg)* Math.sin(ascendingNodeAngle))
+    z = orbCoords.x*(Math.sin(periapsisArg)*Math.sin(inclination)) + orbCoords.y*(Math.cos(periapsisArg)*Math.sin(inclination))
+
+    xSpeed = orbCoords.xSpeed*(Math.cos(periapsisArg)*Math.cos(ascendingNodeAngle) - Math.sin(periapsisArg)*Math.cos(inclination)*Math.sin(ascendingNodeAngle))
+        - orbCoords.ySpeed*(Math.sin(periapsisArg)*Math.cos(ascendingNodeAngle) + Math.cos(periapsisArg)*Math.cos(inclination)*Math.sin(ascendingNodeAngle))
+    ySpeed = orbCoords.xSpeed*(Math.cos(periapsisArg)*Math.sin(ascendingNodeAngle) + Math.sin(periapsisArg)*Math.cos(inclination)*Math.cos(ascendingNodeAngle))
+        + orbCoords.ySpeed*(Math.cos(periapsisArg)* Math.cos(inclination)* Math.cos(ascendingNodeAngle) - Math.sin(periapsisArg)* Math.sin(ascendingNodeAngle))
+    zSpeed = orbCoords.xSpeed*(Math.sin(periapsisArg)*Math.sin(inclination)) + orbCoords.ySpeed*(Math.cos(periapsisArg)*Math.sin(inclination))
+
+    CartesianCoord.new(x,y,z,xSpeed,ySpeed,zSpeed)
+end
+
+class CartesianCoord
+    attr_reader :x, :y, :z, :xSpeed, :ySpeed, :zSpeed
+    def initialize(x,y,z,xSpeed, ySpeed, zSpeed)
+        @x=x
         @y=y
         @z=z
+        @xSpeed=xSpeed
+        @ySpeed=ySpeed
+        @zSpeed=zSpeed
     end
-end
 
-class OrbitalElement
-    attr_reader :ellipse, :longAscNode, :inclin, :perihelArg, :meanAnom
-
-    def initialize(ellipse, longAscNode, inclin, perihelArg, meanAnom)
-        @ellipse = ellipse
-        @longAscNode = longAscNode
-        @inclin = inclin
-        @perihelArg = perihelArg
-        @meanAnom = meanAnom
-    end
 
 end
 
-
-class Ellipse
-
-    attr_reader :centerX, :centerY, :semiminorAxis, :semimajorAxis, :eccentricity, :semifociDist
-
-    def initialize(semiminorAxis, semimajorAxis, centerX=0, centerY=0)
-        @semiminorAxis = semimajorAxis
-        @semimajorAxis = semimajorAxis
-        @eccentricity = Math.sqrt(1-(semiminorAxis/semimajorAxis)**2)
-        @semifociDist = semimajorAxis*@eccentricity
-        @centerX = centerX
-        @centerY = centerY
-    end
-
-end
-
-class Trajectory
-    attr_reader :orbitElem, :perihelLong, :meanLong, :perihelDist, :aphelDist, :orbitalPeriod,
-    :perihelTime, :currentPos,
-    #Next needs the current position to be calculated
-    :trueAnom, #angle between the foci where the ReferenceCelestialBody is, the perihelion position and the actual position of the celestialBody
-    :eccentricAnom #https://en.wikipedia.org/wiki/Eccentric_anomaly
-    def initialize(orbitElem, perihelTime, currentPos)
-        @orbitElem = orbitElem
-        @perihelLong = orbitElem.longAscNode + orbitElem.perihelArg
-        @meanLong = orbitElem.perihelArg+orbitElem.meanAnom
-        @perihelDist = orbitElem.ellipse.semimajorAxis*(1-orbitElem.ellipse.eccentricity)
-        @aphelDist = orbitElem.ellipse.semimajorAxis*(1+orbitElem.ellipse.eccentricity)
-        @orbitalPeriod = orbitElem.ellipse.semimajorAxis ** (1.5)
-        @perihelTime = perihelTime
-        @trueAnom = :noCalculationYet
-        @eccentricAnom = :noCalculationYet
-    end
+class KeplerCoord
+    attr_reader :semiMajorAxis, :eccentricity, :inclination, :ascendingNodeAngle, :periapsisArg, :mainAnom
     
+    def initialize(semiMajorAxis, eccentricity, inclination, ascendingNodeAngle, periapsisArg, mainAnom)
+        @semiMajorAxis = semiMajorAxis
+        @eccentricity = eccentricity, 
+        @inclination = inclination
+        @ascendingNodeAngle = ascendingNodeAngle 
+        @periapsisArg = periapsisArg
+        @mainAnom = mainAnom
+    end
+
+    def toCartesian(referentialBody)
+        eccentricAnomaly = eccentricAnomaly(@mainAnom, @eccentricity)
+        trueAnomaly = trueAnomaly(eccentricAnomaly,eccentricity)
+        distanceFromCentralBody = distanceFromCentralBody(@eccentricity,@semiMajorAxis, eccentricAnomaly)
+        orbitalCoords = orbitalFrameCartesianCoord(@semiMajorAxis, trueAnomaly, distanceFromCentralBody,mu(referentialBody),eccentricAnomaly)
+        
+        bodyCentricFrameCartesianCoord(orbitalCoords,@inclination,@periapsisArg,@ascendingNodeAngle)
+    end
 end
 
-p x = Trajectory.new(OrbitalElement.new(Ellipse.new(1,1,0,0),3,0,0,1),absoluteDayFromEpoch(2000,1,3),CartPosition.new(1,0,-1))
