f8c74298972746026e299b121724bad1eb58aa8c
NwaitDev <nathangallone@hotmail.com>
2022-03-08 15:59:37 +0100

    add toString et Format pr meilleur affich sur map

--- vanilla/view/VisibleBody.java
+++ vanilla/view/VisibleBody.java
@@ -7,9 +7,11 @@ public class VisibleBody {
     
     private static final double RATIO_FARTHEST = 0.8;
     private CelestialBody actualCelestialBody;
+    private static final int DEFAULT_TEXT_WIDTH = 30;
     private int xPos;
     private int yPos;
     private int diameter;
+    public static VisibleBody selected = null;
 
 
     public VisibleBody(MapPanel mp, CelestialBody body){
@@ -28,6 +30,10 @@ public class VisibleBody {
 
             System.out.println(body.getName()+" : Position a l'écran -> ("+xPos+","+yPos+")");
         }
+    }
+
+    public String toString(){
+        return actualCelestialBody.getName()+" ("+xPos+","+yPos+")";
     }
 
     public int getDiameter() {
@@ -51,13 +57,39 @@ public class VisibleBody {
         return yPos;
     }
 
+    /**
+     * action plaçant des retours à la ligne dans une chaîne de caractères
+     * @param txt la chaîne à formater
+     * @param maxWidth le nombre de caractères minimum avant un retour à la ligne
+     */
+    private String format(String txt, int maxWidth){
+        if (txt==null) {
+            return "no text";
+        }
+        int ptr = 0;
+        StringBuffer txtBuffer = new StringBuffer(txt);
+        int l = txtBuffer.length();
+        for(int i = 0; i<l; i++){
+            if(ptr>maxWidth){
+                if(txtBuffer.charAt(i)==' '){
+                    txtBuffer.insert(i+1,'\n');
+                    ptr = 0;
+                }
+            }else{
+                ptr++;
+            }
+        }
+        return txtBuffer.toString();
+    }
 
     public String usefullDataToString() {
-        return "Name : "+actualCelestialBody.getName() +
+        return format("Name : "+actualCelestialBody.getName() +
                 "\nDiameter : "+actualCelestialBody.getDiameter()+
                 "\nAverage distance to "+actualCelestialBody.getReferenceFrame().getName()+" : "+
                 actualCelestialBody.getDistanceFromOrigin()+
-                "\nPeriod of revolution : "+actualCelestialBody.getPeriodOfRevolution()+" earth days\n"+
-                "Period of rotation : "+actualCelestialBody.getPeriodOfRotation();
+                "\nPeriod of revolution around "+actualCelestialBody.getReferenceFrame().getName()+" : "+actualCelestialBody.getPeriodOfRevolution()+" earth days\n"+
+                "Period of rotation : "+actualCelestialBody.getPeriodOfRotation()+" hours",DEFAULT_TEXT_WIDTH);
     }
+
+
 }
