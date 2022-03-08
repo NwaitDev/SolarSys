85fa052deea9a768484c7b48e27e2fc96dd2bd5f
NwaitDev <nathangallone@hotmail.com>
2022-03-08 16:30:00 +0100

    retrait programmes exemples jruby

--- JRuby examples/second-jruby.rb
+++
@@ -1,9 +0,0 @@
-#  ============================================================================
-#  Example of importing a Java class suitable for Ruby ---
-#  Seminex, 15th May 2014
-
-include Java
-
-$a = Java::jrubytrying.Imported_By_JRuby.make
-
-p $a
